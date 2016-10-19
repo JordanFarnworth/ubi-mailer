4a8ff3b172a3ff21dee7495661583b68e8742b8e
Jordan Farnworth <jordanfarn23@gmail.com>
2016-10-19 08:45:17 -0600

    added callback

--- app/controllers/mailing_controller.rb
+++ app/controllers/mailing_controller.rb
@@ -3,7 +3,7 @@ class MailingController < ApplicationController
   def send_default
     UbiMailer.default_email(email_params).deliver_now
     response.headers['Access-Control-Allow-Origin'] = 'https://jordanfarnworth.github.io'
-    render json: {success: "email will be delivered"}, status: :ok
+    render json: {success: "email will be delivered"}, status: :ok, :callback => params[:callback]
   end
 
   private
